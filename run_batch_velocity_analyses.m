c4a6b18c16403800c70a53ec14e8f77a98edcec9
Frank Charbonier <frank.charbonier13@gmail.com>
2023-05-30 14:38:21 -0700

    Create run_batch_velocity_analyses.m

---
+++ run_batch_velocity_analyses.m
@@ -0,0 +1,131 @@
+% Running velocity analyses as batch
+% Choose which functions to run on images in current working directory
+% Frank Charbonier, Stanford University, 2023
+clear;
+close all;
+clc;
+
+run_cell_FIDIC = 0;         
+run_cell_vel = 0;
+run_cell_kym = 1;
+run_cell_compute_traj = 0;
+run_cell_plot_traj = 0;
+run_cell_MSD = 0;
+run_cell_vel_corr = 0;
+run_beads_FIDIC = 0;
+run_compute_tractions = 0;
+run_plot_tractions = 0;
+
+%% USER INPUTS
+% Input image info
+cellname = 'cells.tif';
+domainname = 'domain.tif';
+% Images to run
+image_seq = [];
+% image_seq = 2:30;
+% Frames to include for cell trajectories and MSD (0 to 1)
+nstart= 0;
+nend = 1;
+% Trajectory data from compute_cell_trajectories.m
+trajname = 'cell_trajectories_tstart_end.mat';
+% Output file info
+tract_dirname = 'displ_traction'; % Name of a folder to put traction plots in
+% How much to crop substrate displacement data (default is 10)
+crop_val = 10;
+% Set to zero to skip drift-correction (if previously applied before FIDIC)
+correct_drift = 0;
+%Plotting limits for cell velocity plots and kymographs
+min_vel = -0.25;
+max_vel = 0.25;
+% Plotting limits for substrate displacements and tractions
+umax = 1;     % um
+tmax = 500;      % Pa
+
+%% Run FIDIC on cell image
+if run_cell_FIDIC
+    w0 = 16;
+    d0 = w0/4;
+%     run_FIDIC(fname_ref,fname_multipage,savename,w0,d0,inc,image_seq);
+    run_FIDIC([],cellname,'cells_DIC_results_w0=16.mat',w0,d0,1,image_seq);
+end
+
+%% Compute cell velocities
+if run_cell_vel
+    disp("Computing cell velocities")
+    plot_cellvel(max_vel=max_vel);
+%     TO ADD: separate functions for computing and plotting the processed velocity data
+%       TO ADD: input min_vel and max_vel for plotting
+    disp("Computing cell velocities finished")
+end
+
+%% Plot kymographs of cell velocity
+if run_cell_kym
+    disp("Plotting kymographs of cell velocities")
+    % plot_cell_vel_kymograph(processed_vel_data, min_vel, max_vel)
+    plot_cell_vel_kymograph('cellvel_processed.mat', min_vel, max_vel);
+end
+
+%% Compute cell trajectories
+if run_cell_compute_traj
+    % compute_cell_trajectories(DICname, domainname, cellname, fd, isisland, savename, thr);
+    compute_cell_trajectories();
+end
+
+%% Plot cell velocity MSD
+if run_cell_MSD
+    % plot_MSD(trajname, nstart, nend, savename_plot, savename_data, insivisble)
+%     MSD_savename_plot = ['MSD_' num2str(nstart,2) '_' num2str(nend,2)];
+%     MSD_savename_data = [MSD_savename_plot + ".mat"];
+%     plot_MSD('cell_trajectories_tstart_end.mat', nstart, nend, ...
+%         MSD_savename_plot, MSD_savename_data);
+        plot_MSD(trajname,nstart,nend,'MSD_plot_nstart_nend','MSD_nstart_nend.mat')
+        plot_MSD(trajname, nstart, nend);
+        disp("MSD calculation completed")
+end
+
+%% Plot cell trajectories
+if run_cell_plot_traj
+    % plot_cell_trajectories(traj_name, nstart, nend, savename, invisible)
+    % cell_traj_savename = ["Trajectories_" + num2str(nstart,2) + "_" + num2str(nend,2)];
+    plot_cell_trajectories(trajname,nstart,nend,'Trajectories_nstart_nend');
+    disp("Cell trajectory plotting complete")
+end
+
+%% Plot cell velocity correlation length
+if run_cell_vel_corr
+    % vel_autocorr_nogrid(fname)
+    vel_autocorr_nogrid();
+end
+
+%% Run FIDIC for bead image
+if run_beads_FIDIC
+    fname_ref = [];
+    fname_multipage = 'beads.tif';
+    savename = 'beads_DIC_results_w0=16.mat';
+    w0 = 16;
+    d0 = w0/4;
+    inc = 1;
+    % image_seq = 2:30;
+    image_seq = [];
+    run_FIDIC(fname_ref,fname_multipage,savename,w0,d0,inc,image_seq);
+end
+
+%% Compute cell-substrate tracitions using run_reg_fourier_TFM.m
+if run_compute_tractions
+    disp("Computing tractions")
+    % run_reg_fourier_TFM(filename, savename, domainname, num_images, crop_val, correct_drift)
+    run_reg_fourier_TFM('beads_DIC_results_w0=16.mat', 'tract_results.mat', ...
+        domainname, [], crop_val, correct_drift);
+    disp("Computing tractions complete")
+end
+
+%% Plot tractions
+    
+if run_plot_tractions
+    disp("Plotting tractions")
+%     plot_displ_tractions(cellname, filename, domainname, dirname, savenameheader, umax, tmax, ...
+%         num_images, invisible)
+    plot_displ_tractions(cellname, 'tract_results.mat', domainname, 'displ_traction', ...
+        [tract_dirname,'/t_'], umax, tmax);
+    disp("Plotting tractions complete")
+end

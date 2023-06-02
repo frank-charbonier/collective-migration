04e7c20dfd196de6a85a77f5851560be9a6b4ba9
Frank Charbonier <frank.charbonier13@gmail.com>
2023-06-02 16:34:53 -0700

    Create loop_through_datasets.m

---
+++ loop_through_datasets.m
@@ -0,0 +1,17 @@
+% Loop through datasets in directory
+
+% Get list of files and folders in current directory with 'XY' in the name
+baseFolder = pwd;
+folderInfo = dir('*XY*');
+folderList = {folderInfo.name};
+% celldisp(folderList);
+
+for k = 1:length(folderList)
+    cd(baseFolder)
+    cd(folderList{k});
+    disp(pwd);
+    run_batch_analyses;
+    disp('Folder analysis complete');
+end
+
+

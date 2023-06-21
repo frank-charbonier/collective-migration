d29a7434b640d3e662880bc9508efa57f697c48f
Frank Charbonier <frank.charbonier13@gmail.com>
2023-06-21 16:35:42 -0700

    Update create_kymograph.m

--- create_kymograph.m
+++ create_kymograph.m
@@ -1,6 +1,9 @@
-function [kymo] = create_kymograph(data, binsize, num_Naan_thr)
+function [kymo] = create_kymograph(data, plot_radial, binsize, num_Naan_thr)
     arguments
         data;
+        % Set plot_radial to 1 for radially expanding clusters, or to 0 for rectangular clusters
+        % expanding horizontally to the right
+        plot_radial = 1;
         % bins for spatial averaging
         binsize=1;
         % threshold for including given distance from centroid
@@ -18,44 +21,86 @@ function [kymo] = create_kymograph(data, binsize, num_Naan_thr)
 firstImageSlice=data(:,:,1); %look at the first timepoint for image size info
 numberOfSlices=size(data,3); %see how many timepoints are present
 
-%make a distance map that returns the distance of every measurement from the 
-%tissue centroid (here tissues are segmented with the original tissue 
-%centroid also corresponding to the image centroid.
-thisCentroid=size(firstImageSlice)/2+0.5;
-% thisCentroid =[120,150];
-% thisCentroid=[xc, yc];
-[Y,X]=meshgrid(1:size(firstImageSlice,1),1:size(firstImageSlice,2));
-disFromCentroidYX=sqrt((X-thisCentroid(2)).^2+(Y-thisCentroid(1)).^2); 
+if plot_radial
+    %make a distance map that returns the distance of every measurement from the 
+    %tissue centroid (here tissues are segmented with the original tissue 
+    %centroid also corresponding to the image centroid.
+    thisCentroid=size(firstImageSlice)/2+0.5;
+    % thisCentroid =[120,150];
+    % thisCentroid=[xc, yc];
+    [Y,X]=meshgrid(1:size(firstImageSlice,1),1:size(firstImageSlice,2));
+    disFromCentroidYX=sqrt((X-thisCentroid(2)).^2+(Y-thisCentroid(1)).^2);
+    kymoLength=round(min(thisCentroid-0.5));%set the spatial extent of the kymograph
+    kymo=nan(numberOfSlices,kymoLength); %predefine the kymograph with NaNs
 
-kymoLength=round(min(thisCentroid-0.5));%set the spatial extent of the kymograph
-kymo=nan(numberOfSlices,kymoLength); %predefine the kymograph with NaNs
-tcounter=1; %counter for timeslices
-%outer loop is over timepoints
-for t=1:1:numberOfSlices
-       thisArraySlice=data(:,:,t);
-       rCounter=1;
-       %inner loop is over bins
-       for r=binsize:binsize:kymoLength
+    tcounter=1; %counter for timeslices
+    %outer loop is over timepoints
+    for t=1:1:numberOfSlices
+        thisArraySlice=data(:,:,t);
+        rCounter=1;
+        %inner loop is over bins
+        for r=binsize:binsize:kymoLength
 
-               if r<=binsize %first bin is from the center to the binsize
+            if r<=binsize %first bin is from the center to the binsize
 
-                   kymo(tcounter,1)=nanmean(thisArraySlice((disFromCentroidYX>=0)&(disFromCentroidYX<=binsize)));
-               else %successive bins are between inner and outer sizes
-                    elementIsNan=isnan(thisArraySlice((disFromCentroidYX>r-binsize)&(disFromCentroidYX<=r)));
-                    numberOfElements=length(elementIsNan);
-                    numberOfNans=sum(elementIsNan); 
-                    %numberOfNans returns the number of measurements
-                    %that are outside of the tissue at a given distance
-                    %from the centroid
-                   if numberOfNans/numberOfElements<=num_Naan_thr
-                       %do not record a value on the kymograph for a given 
-                       %distance from the centroid if more than 75% of the
-                       %points at that distance are outside the extent of 
-                       %the tissue
-                        kymo(tcounter,rCounter)=nanmean(thisArraySlice((disFromCentroidYX>r-binsize)&(disFromCentroidYX<=r)));
-                   end
-               end
-           rCounter=rCounter+1;
-       end
-    tcounter=tcounter+1;
+                kymo(tcounter,1)=nanmean(thisArraySlice((disFromCentroidYX>=0)&(disFromCentroidYX<=binsize)));
+            else %successive bins are between inner and outer sizes
+                elementIsNan=isnan(thisArraySlice((disFromCentroidYX>r-binsize)&(disFromCentroidYX<=r)));
+                numberOfElements=length(elementIsNan);
+                numberOfNans=sum(elementIsNan);
+                %numberOfNans returns the number of measurements
+                %that are outside of the tissue at a given distance
+                %from the centroid
+                if numberOfNans/numberOfElements<=num_Naan_thr
+                    %do not record a value on the kymograph for a given
+                    %distance from the centroid if more than 75% of the
+                    %points at that distance are outside the extent of
+                    %the tissue
+                    kymo(tcounter,rCounter)=nanmean(thisArraySlice((disFromCentroidYX>r-binsize)&(disFromCentroidYX<=r)));
+                end
+            end
+            rCounter=rCounter+1;
+        end
+        tcounter=tcounter+1;
+    end
+else
+    % If plot_radial is 0, make a distance map using the x-coordinate of each measurement
+    [Y,X]=meshgrid(1:size(firstImageSlice,1),1:size(firstImageSlice,2));
+    disFromCentroidYX=Y;
+    kymoLength=size(firstImageSlice,2);%set the spatial extent of the kymograph
+    kymo=nan(numberOfSlices,kymoLength); %predefine the kymograph with NaNs
+
+    tcounter=1; %counter for timeslices
+    %outer loop is over timepoints
+    for t=1:1:numberOfSlices
+        thisArraySlice=data(:,:,t);
+        rCounter=1;
+        %inner loop is over bins
+        for y=binsize:binsize:kymoLength
+
+            if y<=binsize %first bin is from the center to the binsize
+
+                kymo(tcounter,1)=nanmean(thisArraySlice((disFromCentroidYX>=0)&(disFromCentroidYX<=binsize)));
+            else %successive bins are between inner and outer sizes
+                % Check for nan measurements 
+                elementIsNan=isnan(thisArraySlice(:,y));
+                numberOfElements=length(elementIsNan);
+                numberOfNans=sum(elementIsNan);
+                %numberOfNans returns the number of measurements
+                %that are outside of the tissue at a given distance
+                %from the centroid
+                if numberOfNans/numberOfElements<=num_Naan_thr
+                    %do not record a value on the kymograph for a given
+                    %distance from the centroid if more than 75% of the
+                    %points at that distance are outside the extent of
+                    %the tissue
+                    kymo(tcounter,rCounter)=nanmean(thisArraySlice(:,y));
+                end
+            end
+            rCounter=rCounter+1;
+        end
+        tcounter=tcounter+1;
+    end
 end
+
+

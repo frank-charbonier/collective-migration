6c32b4c3de5bdaabc4a574024572605e980d7bd5
Frank Charbonier <frank.charbonier13@gmail.com>
2023-06-21 15:53:03 -0700

    Update loop_through_datasets.m

--- loop_through_datasets.m
+++ loop_through_datasets.m
@@ -1,7 +1,12 @@
+% LOOP_THROUGH_DATASETS.M
+% 
+% Loop through all datasets in current directory with 'XY' in the name and call run_batch_analyses.m
+% Frank Charbonier, Stanford University, 2023
+
 % Loop through datasets in directory
 clear
 close all
-clc
+% clc
 
 % Get list of files and folders in current directory with 'XY' in the name
 baseFolder = pwd;

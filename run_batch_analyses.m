0448f576935549910bd9f0c95ae57408bfce5f31
Frank Charbonier <frank.charbonier13@gmail.com>
2023-06-30 13:55:05 -0700

    Update run_batch_analyses.m
    add time counters for each function run

--- run_batch_analyses.m
+++ run_batch_analyses.m
@@ -42,25 +42,31 @@ end
 %% Plot cell velocities
 if run_plot_cell_vel
     disp("Plotting cell velocities")
+    tic
     % plot_cellvel(cellname, domainname, DICname, pix_size, min_vel, max_vel, plot_radial)
     plot_cellvel(cellname, cellvel_savename, pix_size, min_vel, max_vel, plot_radial);
     disp("Plotting cell velocities finished")
+    toc
 end
 
 %% Plot cell velocity summary statistics
 if run_plot_cell_vel_summary
     disp("Plotting cell velocity summary statistics")
+    tic
     % function plot_cellvel_summary(cellvel_savename, time_increment, plot_radial)
     plot_cellvel_summary(cellvel_savename, time_increment, plot_radial)
     disp("Plotting cell velocity summary statistics finished")
+    toc
 end
 
 %% Plot kymographs of cell velocity
 if run_cell_kym
     disp("Plotting kymographs of cell velocities")
+    tic
     % plot_cell_vel_kymograph(processed_vel_data, min_vel, max_vel, savename, plot_radial)
     plot_cell_vel_kymograph(cellvel_savename, min_vel, max_vel, 'Kymographs', plot_radial);
     disp("Plotting kymographs of cell velocities finished")
+    toc
 end
 
 %% Compute cell trajectories
@@ -74,26 +80,34 @@ end
 %% Plot cell velocity MSD
 if run_cell_MSD
     disp("Calculating MSD")
+    tic
     % Set to [] to make figure visible. Set to 1 to make figure invisible.
     invisible = 1;
     % plot_MSD(trajname, nstart, nend, time_increment, savename_plot, savename_data, insivisble)
-    plot_MSD(trajname,nstart,nend,time_increment, 'MSD_plot','MSD_data.mat', invisible)
+    plot_MSD(trajname,nstart,nend,time_increment, 'MSD_plot','MSD_data', invisible)
     disp("MSD calculation completed")
+    toc
 end
 
 %% Plot cell trajectories
 if run_cell_plot_traj
     disp("Plotting cell trajectories")
+    tic
     % plot_cell_trajectories(traj_name, nstart, nend, savename, invisible)
     % cell_traj_savename = ["Trajectories_" + num2str(nstart,2) + "_" + num2str(nend,2)];
     plot_cell_trajectories(trajname,nstart,nend,'Trajectories');
     disp("Plotting cell trajectories finished")
+    toc
 end
 
 %% Plot cell velocity correlation length
 if run_cell_vel_corr
     % vel_autocorr_nogrid(fname)
+    disp("Calculating cell velocity correlation length")
+    tic
     vel_autocorr_nogrid(trajname);
+    disp("Finished calculating cell velocity correlation length")
+    toc
 end
 
 %% Run FIDIC for bead image
@@ -104,18 +118,22 @@ end
 %% Compute cell-substrate tracitions using run_reg_fourier_TFM.m
 if run_compute_tractions
     disp("Computing tractions")
+    tic
     % run_reg_fourier_TFM(filename, savename, domainname, num_images, crop_val, correct_drift)
     run_reg_fourier_TFM('beads_DIC_results.mat', 'tract_results.mat', ...
         domainname, [], crop_val, correct_drift);
     disp("Computing tractions complete")
+    toc
 end
 
 %% Plot tractions
 if run_plot_tractions
     disp("Plotting tractions")
+    tic
 %     plot_displ_tractions(cellname, filename, domainname, dirname, savenameheader, umax, tmax, ...
 %         num_images, invisible)
     plot_displ_tractions(cellname, 'tract_results.mat', domainname, 'displ_traction', ...
         [tract_dirname,'/t_'], umax, tmax);
     disp("Plotting tractions complete")
+    toc
 end

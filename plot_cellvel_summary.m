7afc9db7c2dc5d52fc9356693e0c3a29e44827f4
Frank Charbonier <frank.charbonier13@gmail.com>
2023-06-30 13:54:16 -0700

    Update plot_cellvel_summary.m
    explicitly load cariables from compute_cellvel.m

--- plot_cellvel_summary.m
+++ plot_cellvel_summary.m
@@ -11,8 +11,18 @@ close all;
 %% --- COMPUTE RMS VELOCITIES ---
 savename = 'RMS velocities';
 
-% Load data
-load(cellvel_savename);
+% Explicitly initialize computed velocity data
+% Convert from um/min to um/hr
+if (plot_radial==1)
+    cellvel_data = load(cellvel_savename,"u_cell", "v_cell", "x_cell", "y_cell", "ur", "ut");
+    ur = 60 * cellvel_data.ur;
+    ut = 60* cellvel_data.ut;
+else
+    cellvel_data = load(cellvel_savename,"u_cell", "v_cell", "x_cell", "y_cell");
+end
+u_cell = 60 * cellvel_data.u_cell;
+v_cell = 60* cellvel_data.v_cell;
+
 
 % Loop over all correlations
 K = size(u_cell,3);
@@ -23,7 +33,7 @@ ur_rms = NaN(1,K);
 ut_rms = NaN(1,K);
 div_rms = NaN(1,K);
 
-for k=1:K
+parfor k=1:K
     % Load cell velocity for current frame
     u_cell_k = u_cell(:,:,k);
     v_cell_k = v_cell(:,:,k);
@@ -42,8 +52,6 @@ for k=1:K
         ut_k = ut(:,:,k);
         ur_rms_k = rms(ur_k, "all", "omitnan");
         ut_rms_k = rms(ut_k, "all", "omitnan");
-        
-
     end
 
     % Update values
@@ -79,14 +87,17 @@ print('-dpng','-r300',savename);
 hold off
 
 plot(x_t, div_rms)
-ylabel('RMS of divergence [ \mum/hr ]')
+ylabel('RMS of divergence [hr^{-1}]')
 print('-dpng','-r300', 'Divergence');
+
+% Later add this filename to analysis file
+RMS_cellvel_savename = 'cellvel_summary.mat';
 
 if (plot_radial==1)
     % writematrix(v_rms,'cellvel_summary.txt');
-    save('cellvel_summary.mat', 'u_rms', 'v_rms','u_mag_rms', "ur_rms", "ut_rms");
+    save(RMS_cellvel_savename, 'u_rms', 'v_rms','u_mag_rms', "ur_rms", "ut_rms");
 else
-    save('cellvel_summary.mat', 'u_rms', 'v_rms','u_mag_rms');
+    save(RMS_cellvel_savename, 'u_rms', 'v_rms','u_mag_rms');
 end
 
 close all
